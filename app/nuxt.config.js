40ea09c448a403e55ee2148ed24aaa13b5107c04
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2020-06-02 21:52:29 +0900

    ログイン画面を実装

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -58,20 +58,6 @@ export default {
    */
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
   },
   /*
    ** Build configuration
@@ -80,23 +66,12 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: "pre",
-          test: /\.(js|vue)$/,
-          loader: "eslint-loader",
-          exclude: /(node_modules)/,
-          options: {
-            fix: true
-          }
-        })
-      }
-    }
+    extend(config, ctx) {}
   },
-  devServer: {
-    watchOptions: {
-      poll: true
+  watchers: {
+    webpack: {
+      aggregateTimeout: 300,
+      poll: 1000
     }
   }
 }
---
+++ app/src/pages/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="login">
+    <v-card class="mx-auto mt-5" max-width="400">
+      <v-card-title>
+        <h1 class="display-1">ログイン</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="login">
+          <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" />
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            label="パスワード"
+            @click:append="showPassword = !showPassword"
+          />
+          <v-btn class="info" type="submit">ログイン</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showPassword: false,
+      loginData: {
+        username: null,
+        password: null
+      }
+    }
+  },
+  methods: {
+    login() {}
+  }
+}
+</script>
